452d4058ca6727e3ca3a53e77965c201a51918f3
David Becker <david@beckersite.com>
2018-04-14 14:29:57 -0600

    Setup changeRange in App with state and simple MapView

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import MapView from '../../views/MapView/MapView';
+import MetricsView from '../../views/MetricsView/MetricsView';
 import './App.css';
 
-const App = () =>
-  (
-    <div className="App">
-      <MapView />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      range: {
+        low: 0,
+        high: 0,
+      },
+    };
+    this.changeRange = this.changeRange.bind(this);
+  }
+
+  changeRange(start, end) {
+    this.setState({
+      range: {
+        low: start,
+        high: end,
+      },
+    });
+  }
+
+  render() {
+    const { range } = this.state;
+
+    return (
+      <div className="App">
+        <MetricsView range={range} changeRange={this.changeRange} />
+        <MapView range={range} />
+      </div>
+    );
+  }
+}
 
 export default App;
