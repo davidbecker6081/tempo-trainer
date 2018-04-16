2ea3b6bea67ae67a0d3bd494e7d4cf5d1cb50b5a
David Becker <david@beckersite.com>
2018-04-15 19:01:37 -0600

    Add ShareLinks component with test styling

--- src/components/ShareLinks/ShareLinks.js
+++ src/components/ShareLinks/ShareLinks.js
@@ -4,9 +4,34 @@ import './ShareLinks.css';
 
 const ShareLinks = () =>
   (
-    <article>
-      <img src={logo} alt="Tempo Trainer logo" />
-      <h1>Kyle Heppenstall</h1>
+    <article className="share-links-container">
+      <img className="share-links-logo" src={logo} alt="Tempo Trainer Logo" />
+      <article>
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Facebook Link"
+        >
+          <img src={logo} alt="facebook link" />
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Instagram Link"
+        >
+          <img src={logo} alt="instagram link" />
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Twitter Link"
+        >
+          <img src={logo} alt="twitter link" />
+        </a>
+      </article>
     </article>
   );
 
