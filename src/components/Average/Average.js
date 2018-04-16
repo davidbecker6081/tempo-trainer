2d1c7bfc0b7ca33953170a3f53cf358dd6008587
David Becker <david@beckersite.com>
2018-04-16 12:48:03 -0600

    Add proptypes for Graph

--- src/components/Average/Average.js
+++ src/components/Average/Average.js
@@ -27,8 +27,8 @@ Average.propTypes = {
   channelSet: PropTypes.string.isRequired,
   dataHelper: PropTypes.shape({
     data: PropTypes.shape({}),
-    channels: PropTypes.array,
-    GPSCoords: PropTypes.array,
+    channels: PropTypes.shape([]),
+    GPSCoords: PropTypes.shape([]),
     min: PropTypes.number,
     max: PropTypes.number,
   }).isRequired,
